2d91f91469d0b3006c1d29ff69cfd85f14c455f2
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 16:32:21 +0200

    Add exercises 27-28

--- week-03/day-1/exercise21-30.py
+++ week-03/day-1/exercise21-30.py
@@ -42,4 +42,31 @@ if y[0] == y[-1]:
     out *= 2
 else:
     out /= 2
-print (out)        
+print (out)
+
+z = 13
+# if z is between 10 and 20 print 'Sweet!'
+# if less than 10 print 'More!',
+# if more than 20 print 'Less!'
+if 10 < z and 20 > z:
+    print ("Sweet!")
+elif z < 10:
+    print ("More!")
+else:
+    print ("Less!")
+
+aa = [1, 2, 3]
+out = 0
+# if the aa list contains one element set the out to 1
+# if the aa list contains two element set the out to 2
+# if the aa list contains more than 2 set the out to 10
+# if the aa contains no elements it should set to -1
+if len(aa) == 1:
+    out = 1
+elif len(aa) == 2:
+    out = 2
+elif len(aa) > 2:
+    out = 10
+else:
+    out = -1
+print (out)
