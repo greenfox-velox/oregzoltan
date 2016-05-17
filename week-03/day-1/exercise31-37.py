333297c3c4ff6d4d0b4d33f48bc4fc1717c2a857
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 17:03:26 +0200

    Add exercises 31-32

---
+++ week-03/day-1/exercise31-37.py
@@ -0,0 +1,14 @@
+ad = 6
+# print the numbers till ad from 0
+a = 0
+while a <= ad:
+    print (a)
+    a +=1
+
+ae = 4
+text = 'Gold'
+# print content of the text variable ae times
+a = 0
+while a != ae:
+    print (text)
+    a += 1
