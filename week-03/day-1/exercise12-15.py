316e868e7be3d873029267146e30b94d1cdafc7f
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 15:19:43 +0200

    Fix extension

---
+++ week-03/day-1/exercise12-15.py
@@ -0,0 +1,19 @@
+l = 'python'
+# Tell the 2nd letter of the word
+print (l[1])
+
+m = 'Apple'
+# fill the m variable with its cotnent 4 times
+m = m * 4
+print (m)
+
+n = 'The result is: '
+a = 2
+b = 3
+# concat the product of a and b to the n string
+n = n + str(a) + str(b)
+print (n)
+
+o = "pneumonoultramicroscopicsilicovolcanoconiosis"
+# tell how many letters in o
+print (len(o))
