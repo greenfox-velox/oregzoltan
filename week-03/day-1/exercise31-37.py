544cc170cda1479c067d72dbada9dab65d522efc
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 17:18:23 +0200

    Add exercises 35

--- week-03/day-1/exercise31-37.py
+++ week-03/day-1/exercise31-37.py
@@ -27,3 +27,11 @@ while a != len(ag):
     ag[a] *= 2
     a += 1
 print (ag)
+
+ah = ['kuty', 'macsk', 'cic']
+# add to all elements an 'a' on the end
+a = 0
+while a != len(ah):
+    ah[a] += "a"
+    a += 1
+print (ah)    
