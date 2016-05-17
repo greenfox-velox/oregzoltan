b5a2ab4ec7b8a4a98e5ae49adc4385fc8e780d91
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 16:02:42 +0200

    Add exercises 21-22

---
+++ week-03/day-1/exercise21-30.py
@@ -0,0 +1,10 @@
+u = 123
+# print 'Hooray!' if the number is bigger than 100
+if u > 100:
+    print ("Hooray!")
+
+v = 426
+# print 'Yeah!' if dividable by 4 but print 'End of program' after regardless
+if v % 4 == 0:
+    print ("Yeah!")
+print ("End of program")
