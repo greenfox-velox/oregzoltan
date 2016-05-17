041dffe7bd1ecb15a1bc088f7ffd337c4ada48b8
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 15:44:50 +0200

    Add exercises 16-20

---
+++ week-03/day-1/exercise16-20.py
@@ -0,0 +1,22 @@
+p1 = [1, 2, 3]
+p2 = [4, 5]
+# tell if p2 has more elements than p1
+print (len(p2) > len(p1))
+
+q = [4, 5, 6, 7]
+# get the 3rd element of q
+print (q[2])
+
+r = [54, 23, 66, 12]
+# add the second the third
+print (r[1]+r[2])
+
+s = [1, 2, 3, 8, 5, 6]
+# change the 8 to 4
+s[3] = 4
+print (s)
+
+t = [1, 2, 3, 4, 5]
+# increment the 3rd element
+t[2] += 1
+print (t)
