5643bf0754e00bd0ea86117aef8d1780de5bb6ce
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 16:07:45 +0200

    Add exercises 23-24

--- week-03/day-1/exercise21-30.py
+++ week-03/day-1/exercise21-30.py
@@ -8,3 +8,18 @@ v = 426
 if v % 4 == 0:
     print ("Yeah!")
 print ("End of program")
+
+w = 24
+out = 0
+# if w is even increment out by one
+if w % 2 == 0:
+    out +=1
+print (out)
+
+x = 'monkey'
+# if the string is longer than 4 characters
+# print 'Long!' otherwise print 'Short!'
+if len(x) > 4:
+    print ("Long!")
+else:
+    print ("Short")
