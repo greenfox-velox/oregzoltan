6b26cdfa6235314de54a63ff5ff75ddcbb1d96b6
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-17 16:23:41 +0200

    Add exercises 25-26

--- week-03/day-1/exercise21-30.py
+++ week-03/day-1/exercise21-30.py
@@ -23,3 +23,23 @@ if len(x) > 4:
     print ("Long!")
 else:
     print ("Short")
+
+x = 'cheese'
+# Tell if the x has even or odd number of
+# characters with a True for even and
+# false False output otherwise
+if len(x) % 2 == 0:
+    print (True)
+else:
+    print (False)
+
+y = 'seasons'
+out = 6
+# if the last and the first letter of the string
+# are the same double the variable
+# called out, if not half it
+if y[0] == y[-1]:
+    out *= 2
+else:
+    out /= 2
+print (out)        
