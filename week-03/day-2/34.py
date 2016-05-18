4b0ccf707b21a8328b14f65b51dd6aac2c474786
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-18 12:05:09 +0200

    Add exercise 34

---
+++ week-03/day-2/34.py
@@ -0,0 +1,9 @@
+numbers = [4, 5, 6, 7, 8, 9, 10]
+# write your own sum function
+def sum(numbers):
+    total = 0
+    for a in numbers:
+        total += a
+    return total
+
+print (sum(numbers))
