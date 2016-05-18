4ce9926a151efbd3f7ddb3db4acb14e560c8996f
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-18 09:35:23 +0200

    Add exercise 36-37

--- week-03/day-1/exercise31-37.py
+++ week-03/day-1/exercise31-37.py
@@ -34,4 +34,20 @@ a = 0
 while a != len(ah):
     ah[a] += "a"
     a += 1
-print (ah)    
+print (ah)
+
+ah = [3, 4, 5, 6, 7]
+# print the sum of all numbers in ah
+total = 0
+a = 0
+while a != len(ah):
+    total += ah[a]
+    a += 1
+print(total)
+
+# print the even numbers till 20
+a = 2
+while a < 21:
+    if a % 2 == 0:
+        print(a)
+    a += 1
