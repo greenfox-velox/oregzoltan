19b218f0d56967ee1dcbb68e30f186e7e82f7817
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-20 09:22:49 +0200

    Apply changes

--- week-03/day-3/8.py
+++ week-03/day-3/8.py
@@ -9,18 +9,21 @@ class Person():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.grades = []
 
     def greet(self):
         print(self.first_name, self.last_name)
 
 class Student(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.grades = []
+
     def add_grades(self, grade):
-        self.grades += [grade]
+        self.grades.append(grade)
 
     def salute(self):
         total = 0
-        print("Name: ",self.first_name, self.last_name)
+        self.greet()
         for each in self.grades:
             total += each
         print("Average of grades: ", total/len(self.grades))
