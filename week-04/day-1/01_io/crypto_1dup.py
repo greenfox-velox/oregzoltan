bb89f44d88081325bf8e293b6f6c8c471a17e73c
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-23 15:59:18 +0200

    Add crypto1

---
+++ week-04/day-1/01_io/crypto_1dup.py
@@ -0,0 +1,19 @@
+# Create a method that decrypts the texts/duplicated_chars.txt
+
+def decrypt(file_name):
+    f = open(file_name)
+    text = f.read()
+    text2 = ''
+    counter = 0
+    while counter != len(text):
+        if text[counter] == '\n':
+            text2 += text[counter]
+            counter += 1
+        else:
+            if counter % 2 == 0:
+                counter += 1
+            else:
+                text2 += text[counter]
+                counter += 1
+    f.close()
+    return text2
