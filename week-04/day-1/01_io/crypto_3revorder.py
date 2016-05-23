adcac4aeadd90d79199973121042874d63a4a526
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-23 16:42:50 +0200

    Add crypto3

---
+++ week-04/day-1/01_io/crypto_3revorder.py
@@ -0,0 +1,8 @@
+# Create a method that decrypts texts/reversed_zen_order.txt
+def decrypt(file_name):
+    f = open(file_name)
+    text = ''
+    for line in reversed(f.readlines()):
+        text += line
+    f.close()
+    return text
