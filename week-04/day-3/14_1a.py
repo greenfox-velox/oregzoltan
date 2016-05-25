1bcc1f8018c14cd279f3b170acd308bd16935d4d
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-25 22:57:24 +0200

    Add 14_1a.py

---
+++ week-04/day-3/14_1a.py
@@ -0,0 +1,21 @@
+# reproduce this: [https://github.com/greenfox-velox/velox-syllabus/blob/master/week-04/3-graphics/workshop/r1.png]
+# divide the canvas into 4 parts and repeat the pattern.
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def draw_line(x, y, x2, y2):
+    black_line = canvas.create_line(x, y, x2, y2, fill=color)
+
+step = 20
+color = '#87DE8C'
+for i in range(0, 300, step):
+    draw_line(0, i, i, 300)
+color = '#C175F5'
+for i in range(0, 300, step):
+    draw_line(i, 0, 300 , i)
+root.mainloop()
