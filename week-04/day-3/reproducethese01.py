667898024af50750a5b99db659f5e4c82200fb5a
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-25 12:49:15 +0200

    Add reproducethese01.py

---
+++ week-04/day-3/reproducethese01.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def draw_square(size):
+    x = 2
+    for i in range(1, 20):
+        draw_box = canvas.create_rectangle(x, x, x+size, x+size, fill='#B145F3')
+        x += size
+
+draw_square(10)
+
+root.mainloop()
