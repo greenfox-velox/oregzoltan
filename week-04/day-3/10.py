4cb0263b43a35601b27c2f4d84bf332238ae550a
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-25 11:16:57 +0200

    Add 10.py

---
+++ week-04/day-3/10.py
@@ -0,0 +1,21 @@
+# create a 300x300 canvas.
+# create a square drawing function that takes 1 parameter:
+# the square size
+# and draws a square of that size to the center of the canvas.
+# create a loop that draws 20 squares with that function.
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def draw_square(s):
+    x = 300/2-s/2
+    blue_box = canvas.create_rectangle(x, x, x+s, x+s, fill='blue')
+
+for i in range(20, 1, -1):
+    draw_square(i*10)
+
+root.mainloop()
