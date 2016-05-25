ed5db1375fa1084ab985ffa3ce366b00178bcce4
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-25 14:00:54 +0200

    Add 12.py

---
+++ week-04/day-3/12.py
@@ -0,0 +1,27 @@
+# create a 300x300 canvas.
+# fill it with a checkerboard pattern.
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def draw_square(size):
+    x = 2
+    y = 2
+    for i in range(1, 15):
+        for j in range(1, 15):
+            if (i+j) % 2 == 0:
+                color = 'black'
+            else:
+                color = 'white'
+            draw_box = canvas.create_rectangle(x, y, x+size, y+size, fill=color)
+            x += size
+        x = 2
+        y += size
+
+draw_square(20)
+
+root.mainloop()
