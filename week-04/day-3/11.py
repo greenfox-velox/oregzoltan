963be700fbce119f43362f685100590a3d34da74
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-05-25 11:17:06 +0200

    Add 11.py

---
+++ week-04/day-3/11.py
@@ -0,0 +1,26 @@
+# create a 300x300 canvas.
+# create a square drawing function that takes 2 parameters:
+# the square size, and the fill color,
+# and draws a square of that size and color to the center of the canvas.
+# create a loop that fills the canvas with rainbow colored squares.
+
+from tkinter import *
+import random
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def draw_square(size, color):
+    x = 300/2-size/2
+    draw_box = canvas.create_rectangle(x, x, x+size, x+size, fill=color)
+
+for i in range(300, 1, -1):
+    r1 = random.randrange(10, 99)
+    r2 = random.randrange(10, 99)
+    r3 = random.randrange(10, 99)
+    r = '#' + str(r1) + str(r2) +str(r3)
+    draw_square(i*10, r)
+
+root.mainloop()
