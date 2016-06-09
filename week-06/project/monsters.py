e3667bf7d00eeef7775874c8c0adc528f66dba12
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-09 18:21:44 +0200

    Refactored

--- week-06/project/monsters.py
+++ week-06/project/monsters.py
@@ -1,49 +1,45 @@
-class monster(object):
+import random
+from tkinter import *
+
+class Monster(object):
     def __init__(self, x, y, canvas):
         self.canvas = canvas
-        self.skeleton_image = PhotoImage(file='skeleton.png')
-        self.key = False
-        self.boss = False
         self.x = x
         self.y = y
+        self.key = False
+
+class Skeleton(Monster):
+    def __init__(self, x, y, canvas):
+        super().__init__(x, y, canvas)
+        self.skeleton_image = PhotoImage(file='skeleton.png')
+        self.boss = False
         self.max_hp = 2 * random.randint(1, 6)
         self.hp = self.max_hp
         self.dp = 0.5 * random.randint(1, 6)
         self.sp = random.randint(1, 6)
-        self.stat = 'Monster HP: ' + str(self.hp) + '/' + str(self.max_hp) + ' | DP: ' + str(self.dp) + ' | SP: ' + str(self.sp)
+
+    def stat(self):
+        stat =  'Monster HP: ' + str(self.hp) + '/' + str(self.max_hp) + ' | DP: ' + str(self.dp) + ' | SP: ' + str(self.sp)
+        if self.key == True:
+            stat += ' I have the key'
+        return stat
 
     def draw(self):
         self.canvas.create_image(self.x*60-53, self.y*60-53, image = self.skeleton_image, anchor = NW)
 
-
-class boss(object):
+class Boss(Monster):
     def __init__(self, x, y, canvas):
-        self.canvas = canvas
+        super().__init__(x, y, canvas)
         self.boss_image = PhotoImage(file='boss.png')
-        self.key = False
         self.boss = True
-        self.x = x
-        self.y = y
         self.max_hp = 2 * random.randint(1, 6) + random.randint(1, 6)
         self.hp = self.max_hp
         self.dp = 0.5 * random.randint(1, 6) + random.randint(1, 6)/2
         self.sp = random.randint(1, 6)
-        self.stat = 'Monster HP: ' + str(self.hp) + '/' + str(self.max_hp) + ' | DP: ' + str(self.dp) + ' | SP: ' + str(self.sp)
+
+    def stat(self):
+        stat = 'Boss HP: ' + str(self.hp) + '/' + str(self.max_hp) + ' | DP: ' + str(self.dp) + ' | SP: ' + str(self.sp)
+        return stat
 
     def draw(self):
         self.canvas.create_image(self.x*60-53, self.y*60-53, image = self.boss_image, anchor = NW)
-
-    def create_monsters(self):
-        self.monsters = []
-        self.monsters.append(boss(10, 10, self.canvas))
-        self.map[0].enemy = True
-        for i in range(3):
-            j = True
-            while j == True:
-                ran_x = random.randint(2, 10)
-                ran_y = random.randint(2, 10)
-                for each in self.map:
-                    if each.x == ran_x and each.y == ran_y and each.type == 'floor' and each.enemy == False:
-                        self.monsters.append(monster(ran_x, ran_y, self.canvas))
-                        each.enemy = True
-                        j = False
