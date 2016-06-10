8ca08b155ea385b926fbac220429b9799594af62
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-10 16:43:19 +0200

    Refactored

--- week-06/project/monsters.py
+++ week-06/project/monsters.py
@@ -9,14 +9,14 @@ class Monster(object):
         self.key = False
 
 class Skeleton(Monster):
-    def __init__(self, x, y, canvas):
+    def __init__(self, x, y, canvas, level):
         super().__init__(x, y, canvas)
         self.skeleton_image = PhotoImage(file='skeleton.png')
         self.boss = False
-        self.max_hp = 2 * random.randint(1, 6)
+        self.max_hp = 2 * level * random.randint(1, 6)
         self.hp = self.max_hp
-        self.dp = 0.5 * random.randint(1, 6)
-        self.sp = random.randint(1, 6)
+        self.dp = level/2 * random.randint(1, 6)
+        self.sp = level * random.randint(1, 6)
 
     def stat(self):
         stat =  'Monster HP: ' + str(self.hp) + '/' + str(self.max_hp) + ' | DP: ' + str(self.dp) + ' | SP: ' + str(self.sp)
@@ -28,14 +28,14 @@ class Skeleton(Monster):
         self.canvas.create_image(self.x*60-53, self.y*60-53, image = self.skeleton_image, anchor = NW)
 
 class Boss(Monster):
-    def __init__(self, x, y, canvas):
+    def __init__(self, x, y, canvas, level):
         super().__init__(x, y, canvas)
         self.boss_image = PhotoImage(file='boss.png')
         self.boss = True
-        self.max_hp = 2 * random.randint(1, 6) + random.randint(1, 6)
+        self.max_hp = 2 * level * random.randint(1, 6) + random.randint(1, 6)
         self.hp = self.max_hp
-        self.dp = 0.5 * random.randint(1, 6) + random.randint(1, 6)/2
-        self.sp = random.randint(1, 6)
+        self.dp = level/2 * random.randint(1, 6) + random.randint(1, 6)/2
+        self.sp = level *  random.randint(1, 6) + level
 
     def stat(self):
         stat = 'Boss HP: ' + str(self.hp) + '/' + str(self.max_hp) + ' | DP: ' + str(self.dp) + ' | SP: ' + str(self.sp)
