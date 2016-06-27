d138bef077f3830bfa8f8a1abf454b7b980c3b13
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-27 16:47:21 +0200

    add 36.js

---
+++ week-07/day-1/36.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var ah = [3, 4, 5, 6, 7];
+// print the sum of all numbers in ah
+var i = 0;
+var total = 0;
+while (i < ah.length) {
+  total += ah[i];
+  i++;
+}
+console.log(total);
