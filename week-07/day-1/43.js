a75ba1f2d5acbb1dc50a633e762178e4618e8c1d
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-28 08:59:41 +0200

    add 43.js

---
+++ week-07/day-1/43.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var numbers = [3, 4, 5, 6, 7];
+// write a function that filters the odd numbers
+// from an array and returns a new array consisting
+// only the evens
+function filter_odd(input) {
+  var new_array = [];
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] % 2 === 0) {
+      new_array.push(input[i]);
+    }
+  }
+  return new_array;
+}
+
+console.log(filter_odd(numbers));
