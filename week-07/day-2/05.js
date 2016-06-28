3b080508abad5440c61139b5fb695efa267ca219
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-28 13:01:50 +0200

    add 05.js

---
+++ week-07/day-2/05.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var numbers = [2.4, 3.5, 1.7, 3.3, 1.2];
+
+// create a function that takes an array of numbers,
+// it should return a new array that consists only the numbers that are
+// bigger than 2 and all of it's elements should be rounded
+function biggerThanTwo(nums) {
+  return nums.filter(function(e) {
+    return e > 2;
+  }).map(function(e) {
+    return Math.round(e);
+  });
+}
+console.log(biggerThanTwo(numbers));
