dea64bdfabfd9e2cc0bd734fc156a6ddc3912425
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-28 08:59:13 +0200

    add 40.js

---
+++ week-07/day-1/40.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var aj = 'kuty'
+// write a function that gets a string as an input
+// and appends an 'a' character to its end and returns a new string
+function add_a(string) {
+  var new_string = string + 'a';
+  return new_string;
+}
+
+console.log(add_a(aj));
