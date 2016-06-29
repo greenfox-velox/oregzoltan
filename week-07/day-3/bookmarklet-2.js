9e52871307ccc8a8aa7eb5af484dbd237e37127a
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-29 16:54:52 +0200

    add bookmarklet-2

---
+++ week-07/day-3/bookmarklet-2.js
@@ -0,0 +1,9 @@
+'use strict';
+    // Create a script that replaces every image
+    // with this: http://bit.ly/lpgreenfox
+    // Create a bookmarklet that does that on any website.
+
+var images = document.querySelectorAll('img');
+images.forEach(function(e) {
+  e.setAttribute('src', 'http://bit.ly/lpgreenfox');
+})
