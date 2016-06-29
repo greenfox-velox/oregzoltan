6ea9bba3013758ffca8cc4a94d138ae599c936b2
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-29 12:03:16 +0200

    add content-3

---
+++ week-07/day-3/content-3.js
@@ -0,0 +1,7 @@
+    // fill output1 with the first paragraph's content, text only.
+
+document.querySelector('.output1').innerHTML = document.querySelector('p').textContent;
+
+    // fill output2 with the first paragraph's content keeping the cat strong.
+
+document.querySelector('.output2').innerHTML = document.querySelector('p').innerHTML;
