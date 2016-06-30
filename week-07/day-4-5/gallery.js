6903db1c059387616a0aa50c7317af878b5ee70c
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-30 17:10:40 +0200

    update small buttons

--- week-07/day-4-5/gallery.js
+++ week-07/day-4-5/gallery.js
@@ -10,13 +10,22 @@ var pictureList = [
   'imgs/6109.jpg',
   'imgs/6111.jpg',
   'imgs/6112.jpg',
+  'imgs/6115.jpg',
+  'imgs/6116.jpg',
+  'imgs/6117.jpg',
 ];
+
+var counter = 0;
 var actual = 0;
+var actualSmall = 0;
+var limit = 0;
+
 var img_main = document.querySelector('.img_big');
 img_main.setAttribute('src', pictureList[actual]);
 
 var img_row = document.querySelectorAll('.img_small');
-for (var i = 0; i < 4; i++) {
+limit = actualSmall+4;
+for (var i = actualSmall; i < limit; i++) {
   img_row[i].setAttribute('src', pictureList[i]);
 }
 
@@ -35,6 +44,7 @@ function bigToRight() {
     img_title.textContent = img_main.getAttribute('src');
   }
 }
+
 function bigToLeft() {
   if (actual > 0) {
     actual--;
@@ -43,13 +53,31 @@ function bigToLeft() {
   }
 }
 
+function smallToRight() {
+  if (actualSmall+4 < pictureList.length-1) {
+    actualSmall++;
+    limit = actualSmall+4;
+    counter = 0;
+    for (var i = actualSmall; i < limit; i++) {
+      img_row[counter].setAttribute('src', pictureList[i]);
+      counter++;
+    }
+  }
+}
 
 function smallToLeft() {
-
+  if (actualSmall > 0) {
+    actualSmall--;
+    limit = actualSmall+4;
+    counter = 0;
+    for (var i = actualSmall; i < limit; i++) {
+      img_row[counter].setAttribute('src', pictureList[i]);
+      counter++;
+    }
+  }
 }
-function smallToRight() {
 
-}
+
 buttonBL.addEventListener('click', bigToLeft);
 buttonSL.addEventListener('click', smallToLeft);
 buttonBR.addEventListener('click', bigToRight);
