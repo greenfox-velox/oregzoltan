3d9af3335c9c0d5dcfc69970a4e70b7ff77a2b41
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-07-06 21:04:02 +0200

    add index-candies.js

---
+++ week-08/day-3/index-candies.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var creCan = document.querySelector('.creCan');
+var buyLol = document.querySelector('.buyLol');
+var numCan = document.querySelector('.numCan');
+var numLol = document.querySelector('.numLol');
+var candies = 0;
+var lollipops = 0;
+setCandies();
+setLollipops();
+
+function setCandies() {
+  numCan.textContent = 'Number of candies: ' + candies;
+}
+
+function setLollipops() {
+  numLol.textContent = 'Number of lollipops: ' + lollipops;
+}
+
+function createCandies() {
+  candies++;
+  setCandies();
+}
+
+function buyLollipop() {
+  if (candies >= 100) {
+    candies -= 100;
+    lollipops++;
+    setCandies();
+    setLollipops();
+  }
+}
+
+setInterval(function () {
+  if (lollipops >= 10){
+    candies += Math.floor(lollipops / 10);
+    setCandies();
+  }
+  if (candies === 10000) {
+    alert('WIN!');
+  }
+}, 1000);
+
+
+creCan.addEventListener('click', createCandies);
+buyLol.addEventListener('click', buyLollipop);
