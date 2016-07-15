16f8c935673d2baa2d04a71cfa1b2c59a9740d71
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-07-15 10:45:23 +0200

    put and del works

--- week-09/todos-mongo/todos.js
+++ week-09/todos-mongo/todos.js
@@ -17,22 +17,9 @@ MongoClient.connect(url, function (err, db) {
   } else {
     console.log('Connection established to', url);
 
-    // do some work here with the database.
     var collection = db.collection('todos');
     collection.remove({});
-    // //Create some users
-    // var todo1 = {id: 1, text: 'eat', completed: false};
-    // var todo2 = {id: 2, text: 'drink', completed: false};
-    // var todo3 = {id: 3, text: 'sleep', completed: false};
-    //
-    // // Insert some users
-    // collection.insert([todo1, todo2, todo3], function (err, result) {
-    //   if (err) {
-    //     console.log(err);
-    //   } else {
-    //     console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
-    //   }
-    // });
+
     app.get('/todos', function(req, res) {
       collection.find().toArray(function (err, result) {
         if (err) {
@@ -58,64 +45,43 @@ MongoClient.connect(url, function (err, db) {
     });
 
   app.put('/todos/:id', function(req, res) {
-    collection.update({'id': req.params.id}, {$set:{'completed': true}}, function(err, result){
+    collection.update({'id': parseInt(req.params.id)}, {$set:{'text': req.body.text, 'completed': true}}, function(err, result){
       if(err) {
         console.log(err.toString());
         return;
       }
-    errorHandling(res, {id: +req.params.id, text: req.body.text, completed: req.body.completed});
+    response(res, {id: +req.params.id, text: req.body.text, completed: req.body.completed});
     });
   });
-}});
+
+  app.get('/todos/:id', function(req, res) {
+    collection.find({'id': parseInt(req.params.id)}, function(err,row){
+      if(err) {
+        console.log(err.toString());
+        return;
+      }
+      errorHandling(res, row[0]);
+    });
+  });
 
   app.delete('/todos/:id', function(req, res) {
-    collection.remove({}, function(err,row){
+    collection.remove({'id': parseInt(req.params.id)}, function(err,row){
       if(err) {
         console.log(err.toString());
         return;
       }
-      errorHandling(res, {id: +req.params.id, text: req.body.text, completed: req.body.completed});
+      response(res, {id: +req.params.id, text: req.body.text, completed: req.body.completed});
     });
   });
+  }
+});
 
-function errorHandling(res, item) {
+function response(res, item) {
   if (item === undefined) {
     res.sendStatus(404);
   } else {
     res.send(item);
   }
 }
-app.listen(3000);
 
-//
-// app.get('/todos/:id', function(req, res) {
-//   con.query('SELECT * FROM todo WHERE id =' +req.params.id, function(err,row){
-//     if(err) {
-//       console.log(err.toString());
-//       return;
-//     }
-//     errorHandling(res, row[0]);
-//   });
-// });
-//
-//
-// app.put('/todos/:id', function(req, res) {
-//   con.query('UPDATE todo SET text = "' + req.body.text + '", completed = ' + req.body.completed + ' WHERE id = ' + req.params.id, function(err,row){
-//     if(err) {
-//       console.log(err.toString());
-//       return;
-//     }
-//   errorHandling(res, {id: +req.params.id, text: req.body.text, completed: req.body.completed});
-//   });
-// });
-//
-// app.delete('/todos/:id', function(req, res) {
-//   con.query('DELETE FROM todo WHERE id = ' + req.params.id, function(err,row){
-//     if(err) {
-//       console.log(err.toString());
-//       return;
-//     }
-//     errorHandling(res, {id: +req.params.id, text: req.body.text, completed: req.body.completed});
-//   });
-// });
-//
+app.listen(3000);
